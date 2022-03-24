c13c9ee2c0522e6583926751312985a031dbc213
Stuart Russell <sr895@cam.ac.uk>
2022-03-24 13:39:57 +0000

    Updated __init__.py docstring

--- src/ellipticity/__init__.py
+++ src/ellipticity/__init__.py
@@ -1,2 +1,19 @@
+"""
+Ellipticity toolkit
+========================
+A Python package for the calculation of ellipticity corrections for seismic phases in planetary models
+
+When publishing work produced with this package please cite the following publication:
+INSERT PAPER HERE
+
+
+Functions:
+    calculate_correction               - Calculates ellipticity correction for a given ray path and velocity model
+
+"""
+
+__version__ = "1.1"
+__author__ = "Stuart Russell"
+
 # Import functions
 from .main import calculate_correction
